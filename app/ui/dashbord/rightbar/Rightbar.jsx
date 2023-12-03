8fe19d0defdfc25a7d259d3d44f4789681b60c18
hadi mohammadzadeh <hadi.mohammadzadeh1382@gmail.com>
2023-12-04 02:27:22 +0330

    implement rightbar navbar

--- app/ui/dashbord/rightbar/Rightbar.jsx
+++ app/ui/dashbord/rightbar/Rightbar.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 import Styles from "./rightbar.module.css"
 export default function Rightbar() {
   return (
-    <div>Rightbar</div>
+    <div className='bg-red-100'>Rightbar</div>
   )
 }
