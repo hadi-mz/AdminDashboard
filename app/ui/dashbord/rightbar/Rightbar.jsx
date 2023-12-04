3e41477dcd0f2c6973a7caeb38bf3dd5b97577e1
hadi mohammadzadeh <hadi.mohammadzadeh1382@gmail.com>
2023-12-04 10:39:45 +0330

    change rifght bar bg

--- app/ui/dashbord/rightbar/Rightbar.jsx
+++ app/ui/dashbord/rightbar/Rightbar.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 import Styles from "./rightbar.module.css"
 export default function Rightbar() {
   return (
-    <div className='bg-red-100'>Rightbar</div>
+    <div className='bg-red-300 w-full h-24'>Rightbar</div>
   )
 }
